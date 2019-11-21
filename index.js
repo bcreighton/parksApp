a4d779991f29917f7f6a143850b07bab394bd6e1
Bryson Creighton <brysoncreighton@gmail.com>
2019-11-21 10:09:24 -0600

    added functionality to pull information from the national parks api. Header is not passing correctly; causing errors

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
+const apiUrl = 'https://developer.nps.gov/api/v1/parks';
 const apiKey = 'ZTxAxb3ljD6QPXSqoidY7AMKb8FWxPbjd0KNYj0W';
 
   function getParks() {
     console.log('Parks are being generated...')
 
-    const searchUrl = '';
+    const url = apiUrl + '?stateCode=CO&limit=10&api_key=' + apiKey;
 
-    const options = {
+    /* const options = {
         headers: new Headers({
           "X-Api-Key": apiKey})
-      };
+      }; */
+
+    const fetchParks = async () => {
+      try {
+        const parksRes = await fetch(url);
+        const stateParks = await parksRes.json();
+
+        console.log(stateParks);
+
+      } catch(e) {
+        console.log('something went wrong');
+      }
+    }
+
+    fetchParks();
 }
 
-function watchFrom() {
+function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
-        const parkID = $('#parkField').val();
+
+        console.log('Parks form has been submitted...');
+
         const state = $('#stateSelect').val();
         const maxResults = $('maxResults').val();
+
+        getParks();
+        //getParks(state, maxResults);
     })
 }
+
+$(watchForm);
