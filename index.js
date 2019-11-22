3db40b77545c17c0b6f2aa4aca9d1d1ecfa84f5d
Bryson Creighton <brysoncreighton@gmail.com>
2019-11-22 01:08:29 -0700

    added functionality to handle multiple stats, default max results to 10 if no result limit given by the user and address the park address to the DOM

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+'use strict'
+
 const apiUrl = 'https://developer.nps.gov/api/v1/parks';
 const apiKey = 'ZTxAxb3ljD6QPXSqoidY7AMKb8FWxPbjd0KNYj0W';
 
 function formatParams(params){
+  //sets default limit to 10
+  if(params.limit === ''){
+    params.limit = 10;
+  }
+
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+  const queryString = queryItems.join('&');
+  if(queryString.includes('%2C') || queryString.includes('%20')){
+    return queryString.replace('%2C', '&stateCode=').replace('%20', '');
+  } else {
+    return queryString;
+  }
 }  
 
 function displayParks(stateParks, state) {
@@ -19,6 +31,7 @@ function displayParks(stateParks, state) {
       `<li class="parkItem">
         <h3 class="parkTitle">${stateParks.data[i].fullName}</h3>
         <p class="parkDescription">${stateParks.data[i].description}</p>
+        <p class="parkAddress"><span>Address: </span>${stateParks.data[i].addresses[1].line1} ${stateParks.data[i].addresses[1].city}, ${stateParks.data[i].addresses[1].stateCode} ${stateParks.data[i].addresses[1].postalCode}</p>
         <a href='${stateParks.data[i].url}'><button class="parkBtn">More Details</button></a>
         <a href='${stateParks.data[i].directionsUrl}'><button class="parkBtn">Get Directions</button></a>
       </li>`
@@ -32,6 +45,7 @@ function displayParks(stateParks, state) {
 function getParks(state, maxResults) {
 
     const params = {
+      fields: 'addresses',
       api_key: apiKey,
       stateCode: state,
       limit: maxResults
@@ -55,7 +69,7 @@ function getParks(state, maxResults) {
         $('#errorMessage').empty()
         .html('Something went wrong:' + e);
 
-        $('#errorMessageContainer').removeClass(hidden);
+        $('#errorMessageContainer').removeClass('hidden');
       }
     }
 
